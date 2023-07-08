6032a3074cf1dbd77ba75241bf35ab1474e0c4a0
Reekin <Gazereekin@foxmail.com>
2023-07-08 23:43:09 +0800

    Update ActionSplitter.py

--- ActionSplitter.py
+++ ActionSplitter.py
@@ -23,26 +23,25 @@ for i, frame_split in enumerate(frame_splits):
 
     #Create new action
     new_action = bpy.data.actions.new(name=f"plw_LongSword_100_{i+1}")
-	print(f"{new_action.name}")
+    print(f"{new_action.name}")
 	
     #Copy key_points
     for j, fcurve_info in enumerate(sorted_fcurves_info):
-		print(f"copying {i},{j}: {time.perf_counter()}")
+        print(f"copying {i},{j}: {time.perf_counter()}")
         new_fcurve = new_action.fcurves.new(data_path=fcurve_info["data_path"], index=fcurve_info["array_index"])
-
         for keyframe_point in fcurve_info["keyframe_points"]:
-			print(f"keyframe_point.co.x = {keyframe_point.co.x}, offset = {offset}")
             if keyframe_point.co.x >= frame_split:
                 break
             new_point = new_fcurve.keyframe_points.insert(frame=int(keyframe_point.co.x) - offset, value=keyframe_point.co.y) #Simply set value to the new one, some infos may be missed.
-			#new_point.xxx = keyframe_point.xxx
+	    #new_point.xxx = keyframe_point.xxx
 
-	print(f"copy finished {i}: {time.perf_counter()}")
+    print(f"copy finished {i}: {time.perf_counter()}")
 	
     for j,fcurve_info in enumerate(sorted_fcurves_info):
-		print(f"removing {i},{j}: {time.perf_counter()}")
-		fcurve_info["keyframe_points"] = [p for p in fcurve_info["keyframe_points"] if p.co.x >= frame_split]
+        print(f"removing {i},{j}: {time.perf_counter()}")
+	fcurve_info["keyframe_points"] = [p for p in fcurve_info["keyframe_points"] if p.co.x >= frame_split]
 			
-	print(f"remove finished {i}: {time.perf_counter()}")
-	offset = frame_split
-	print(f"offset = {offset}")
+    print(f"remove finished {i}: {time.perf_counter()}")
+    offset = frame_split
+	
+print(f"Finished!")
